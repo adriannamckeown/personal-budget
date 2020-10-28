a923ff880ec5c9737ce8a7093b9f47b16e1fa1b6
Adrianna McKeown <adrianna_mckeown@live.com>
2020-10-28 19:45:39 -0400

    D3 correction

--- server.js
+++ server.js
@@ -4,41 +4,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const budget = require('./budget.json')
+
+app.use('/', express.static('public'));
 
 app.use(cors());
-
-const budget = { 
-        myBudget: [
-        {
-            title: 'Eat out',
-            budget: 45
-        },
-        {
-            title: 'Rent',
-            budget: 320
-        },
-        {
-            title: 'Grocery',
-            budget: 150
-        },
-        {
-            title: 'Clothes',
-            budget: 100
-        },
-        {
-            title: 'Entertainment',
-            budget: 45
-        },
-        {
-            title: 'Transportation',
-            budget: 75
-        },
-        {
-            title: 'Utilities',
-            budget: 150
-        }
-    ]
-};
 
 app.get('/budget', (req, res) => {
     res.json(budget);
